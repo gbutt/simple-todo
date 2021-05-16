4b7a45006f16d420bbbc892c1f82bd696f96c706
Greg Butt <greg.butt@scaledagile.com>
2021-05-15 18:51:36 -0600

    remvoe body-parser

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import bodyParser from 'body-parser';
 import TodoRoutes from './todo-routes';
 
 const APP_PORT = 3001;
@@ -14,43 +13,49 @@ const compiler = webpack({
     rules: [
       {
         test: /\.js$/,
-        include: [path.join(__dirname, '../frontend'), path.join(__dirname, '../node_modules/@salesforce/design-system-react')],
+        include: [
+          path.join(__dirname, '../frontend'),
+          path.join(__dirname, '../node_modules/@salesforce/design-system-react')
+        ],
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(svg|gif|jpe?g|png)$/,
         use: {
-          loader: 'url-loader?limit=10000',
-        },
+          loader: 'url-loader?limit=10000'
+        }
       },
       {
         test: /\.(eot|woff|woff2|ttf)$/,
         use: {
-          loader: 'url-loader?limit=30&name=assets/fonts/webfonts/[name].[ext]',
-        },
-      },
-    ],
+          loader: 'url-loader?limit=30&name=assets/fonts/webfonts/[name].[ext]'
+        }
+      }
+    ]
   },
   output: {
     filename: 'app.js',
-    path: '/',
-  },
+    path: '/'
+  }
 });
 
 const app = new WebpackDevServer(compiler, {
   contentBase: '/public/',
   publicPath: '/frontend/',
-  stats: { colors: true },
+  stats: { colors: true }
 });
 
-app.use(bodyParser.json());
-app.use('/slds', express.static(path.resolve(__dirname, '../node_modules/@salesforce-ux/design-system/')));
+app.use(express.json());
+app.use(
+  '/slds',
+  express.static(path.resolve(__dirname, '../node_modules/@salesforce-ux/design-system/'))
+);
 app.use('/', express.static(path.resolve(__dirname, '../public')));
 app.use(TodoRoutes);
 
